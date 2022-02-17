c0b56f7632d9f21ba03627a0f2cd0f1c16d71119
pajith551@gmail.com <pajith551@gmail.com>
2022-02-18 01:12:57 +0530

    prompt name

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-var a = 1;
-var b = 2;
-console.log(a+b);
-alert("hello");
-window.alert("Javascript");
-console.log(typeof(123));
-prompt("Enter your name");
+// var a = 1;
+// var b = 2;
+// console.log(a+b);
+// alert("hello");
+// window.alert("Javascript");
+// console.log(typeof(123));
+// prompt("Enter your name");
+
+var myName = "hello";
+var yourName = prompt("Enter your name");
+// console.log(yourName);
+
+console.log("i am" + " " + myName +", welcome to my course" + yourName);
