985d29d1a7f0420b72c9f69a8d7573e6d6fa2ffa
pajith551@gmail.com <pajith551@gmail.com>
2022-02-18 22:01:29 +0530

    naming

--- script.js
+++ script.js
@@ -6,8 +6,17 @@
 // console.log(typeof(123));
 // prompt("Enter your name");
 
-var myName = "hello";
-var yourName = prompt("Enter your name");
-// console.log(yourName);
+// var myName = "hello";
+// var yourName = prompt("Enter your name");
+// // console.log(yourName);
 
-console.log("i am" + " " + myName +", welcome to my course" + yourName);
+// console.log("i am" + " " + myName +", welcome to my course" + yourName);
+
+var a = 3;
+var b = 8;
+
+var c = a;
+var a = b;
+var b = c
+console.log("a is"+ " " + a);
+console.log("b is"+ " " + b);
